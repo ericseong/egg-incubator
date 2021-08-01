fb156d8a94fa14ea8ab253113aca3976c9290dc0
ericseong <eric.seong.2010@gmail.com>
2021-08-01 15:27:44 +0900

    tmp117_test.cpp was written and verified.

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ egg-incubator program.
 
 # TODO
 
+# Dependencies
+The followings the additional configurations I did for this project via raspi-config
+* Enable I2C
+* Enable SPI
+
+The following libraries has been installed during this project
+* Git
+* WiringPi (requires to install from source)
+* i2c-tools
+
+Some useful info.
+* i2c address for TMP117: 0x48
+* i2c address for RTC module: 0x68 
+
+// EOF
+
---
+++ test/tmp117/tmp117_test.cpp
@@ -0,0 +1,47 @@
+// tmp117 h/w test
+// how to build: g++ tmp117_test.cpp -std=c++14 -lwiringPi -o tmp117_test 
+
+#include <iostream>
+#include <iomanip>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+#include "../../../SparkFun_TMP117_Arduino_Library/src/SparkFun_TMP117_Registers.h"
+
+using namespace std;
+
+#define I2CADDR_TMP117 0x48
+#define TEMP_RES 7.8125E-3 // celcius degree
+#define SWAP_BYTE(x) ( ( x<<8 & 0xff00 ) | ( x>>8 & 0x00ff ) )
+
+int main() {
+	int fd;
+
+	fd = wiringPiI2CSetup(I2CADDR_TMP117);
+	cout << "Init result: " << fd << endl;
+
+	cout << "Read result: " << endl;
+	for( int i = 0 ; i < 60 ; i++ ) {
+		short data = wiringPiI2CReadReg16( fd, TMP117_TEMP_RESULT );
+		if( data == -1 ) {
+			cout << "Read failed." << endl;
+		} else {
+			short data1 = SWAP_BYTE(data);	
+
+			float cel;
+			if( data1 & 0x8000 ) { // negative degrees
+				cel = -256.0 + ( data1 & 0x7fff ) * TEMP_RES; 
+			} else {
+				cel = data1 * TEMP_RES; 
+			}
+			cout << std::fixed << std::setw( 5 ) << std::setprecision( 2 ) << cel << " @C " ;
+			cout << "Raw data: " << "0x" << std::hex << data1 << endl;
+		}
+		delay( 1000 ); // 1 sec 
+	}
+
+	return 0;
+}
+
+// eof
+
