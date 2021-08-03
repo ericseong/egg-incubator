43c0a32c29a9a28d52ae9d1e4998227949f5d442
eric seong <eric.seong.2010@gmail.com>
2021-08-03 17:47:52 +0900

    gpio.h is created.

---
+++ src/gpio.h
@@ -0,0 +1,14 @@
+// gpio pin configuration for rpi zero
+
+#ifndef __GPIO_H__
+#define __GPIO_H__ 
+
+// gpio pin numbering by wiringPi configuration 
+#define PWMFAN1_PIN 					26 /* GPIO-12 */
+#define PWMFAN2_PIN 					23 /* GPIO-13 */
+#define RELAY_BULB_PIN 				21 /* GPIO-5 */
+#define RELAY_HUMIDIFIER_PIN 	22 /* GPIO-6 */
+#define RELAY_MOTOR_PIN 			27 /* GPIO-16 */
+
+#endif
+
