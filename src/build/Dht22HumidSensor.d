ecaa7fd54574ba8ecc2414a69e574baeb54a32b5
ericseong <eric.seong.2010@gmail.com>
2021-08-11 01:51:24 +0900

    empty build/ is added to track.

--- src/build/Dht22HumidSensor.d
+++
@@ -1,2 +0,0 @@
-Dht22HumidSensor.o build/Dht22HumidSensor.d : Dht22HumidSensor.cpp /usr/include/stdc-predef.h \
- Dht22HumidSensor.h Sensor.h
--- src/build/Dht22TempSensor.d
+++
@@ -1,2 +0,0 @@
-Dht22TempSensor.o build/Dht22TempSensor.d : Dht22TempSensor.cpp /usr/include/stdc-predef.h \
- Dht22TempSensor.h Sensor.h
