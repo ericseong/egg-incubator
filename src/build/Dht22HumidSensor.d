26f0cbb3dae30e3ea3bed73fd79f0cbcd9df00d2
ericseong <eric.seong.2010@gmail.com>
2021-08-11 01:49:08 +0900

    src/build is added.

---
+++ src/build/Dht22HumidSensor.d
@@ -0,0 +1,2 @@
+Dht22HumidSensor.o build/Dht22HumidSensor.d : Dht22HumidSensor.cpp /usr/include/stdc-predef.h \
+ Dht22HumidSensor.h Sensor.h
---
+++ src/build/Dht22TempSensor.d
@@ -0,0 +1,2 @@
+Dht22TempSensor.o build/Dht22TempSensor.d : Dht22TempSensor.cpp /usr/include/stdc-predef.h \
+ Dht22TempSensor.h Sensor.h
